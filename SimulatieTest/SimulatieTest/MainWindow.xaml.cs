f76025901b01815ad2c0c04e97ad0f50fc7b6b6c
Kenley Strik <kenley.strik@casema.nl>
2019-09-06 13:27:15 +0200

    Changes to SimulationTest

--- SimulatieTest/SimulatieTest/MainWindow.xaml.cs
+++ SimulatieTest/SimulatieTest/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace SimulatieTest
         {
             InitializeComponent();
 
-            for(int i = 0; i < 100; i++)
+            for(int i = 0; i < 20; i++)
             {
                 WrapPanel wrapPanel = new WrapPanel();
                 Label label = new Label();
@@ -46,8 +46,8 @@ namespace SimulatieTest
             Page25 page25 = new Page25(0x7A, 0x49, 0x32, 0x06, 0x00, 0x33);
 
             bike = new SimulationBike(this);
-            bike.AddPageSimualtion(page16, 10, 1000);
-            bike.AddPageSimualtion(page25, 10, 2000);
+            bike.AddPageSimualtion(page16, 10, 250);
+            bike.AddPageSimualtion(page25, 10, 250);
 
             //Page16 page = new Page16(0x19, 0x66, 0x63, 0x03, 0xFF, 3, 4);
             //Page16 page = new Page16(0x19, 0x68, 0x63, 0x02, 0xFF, 3, 4);
@@ -64,11 +64,32 @@ namespace SimulatieTest
 
         public void ReceiveBikeData(byte[] data, Bike bike)
         {
+            Dictionary<string, int> translatedData = TacxTranslator.Translate(BitConverter.ToString(data).Split('-'));
+
             Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
             {
-                txb_Messages.Text += BitConverter.ToString(data) + " : " + Encoding.UTF8.GetString(data) + Environment.NewLine;
+                //txb_Messages.Text += BitConverter.ToString(data).Replace('-', ' ') + " : " + Encoding.UTF8.GetString(data) + Environment.NewLine;
                 //lbl_Test.Content = BitConverter.ToString(data) + " : " + Encoding.UTF8.GetString(data);
-                txb_Messages.ScrollToEnd();
+                //txb_Messages.ScrollToEnd();
+                List<string> keys = translatedData.Keys.ToList();
+                List<int> values = translatedData.Values.ToList();
+
+                if(values[0] == 16)
+                {
+                    txb_MessagesP16.Text = "";
+                    for (int i = 0; i < values.Count; i++)
+                    {
+                        txb_MessagesP16.Text += keys[i] + ": " + values[i] + Environment.NewLine;
+                    }
+                }
+                else if(values[0] == 25)
+                {
+                    txb_MessagesP25.Text = "";
+                    for (int i = 0; i < values.Count; i++)
+                    {
+                        txb_MessagesP25.Text += keys[i] + ": " + values[i] + Environment.NewLine;
+                    }
+                }
             }));
         }
 
---
+++ SimulatieTest/SimulatieTest/TacxTranslator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatieTest
+{
+    class TacxTranslator
+    {
+        public static Dictionary<string, int> Translate(string[] bits)
+        {
+            try
+            {
+                int dataPage = Convert.ToInt32(bits[4], 16);
+                Console.WriteLine(dataPage);
+
+                if (dataPage == 16)
+                {
+                    return TranslateDataPage16(bits);
+                }
+                else if (dataPage == 25)
+                {
+                    return TranslateDataPage25(bits);
+                }
+
+            }
+            catch (IndexOutOfRangeException e) { }
+
+            return new Dictionary<string, int>();
+        }
+
+        private static Dictionary<string, int> TranslateDataPage16(string[] bits)
+        {
+            Dictionary<string, int> translatedData = new Dictionary<string, int>();
+
+            translatedData.Add("PageID", 16);
+            translatedData.Add("EquipmentTypebitField ", Convert.ToInt32(bits[5], 16));
+            translatedData.Add("time ", Convert.ToInt32(bits[6], 16));
+            translatedData.Add("distance ", Convert.ToInt32(bits[7], 16));
+            translatedData.Add("speed ", Convert.ToInt32(bits[8] + bits[9], 16));
+            translatedData.Add("heartBeat ", Convert.ToInt32(bits[10], 16));
+            translatedData.Add("capabilitiesBitField ", Convert.ToInt32(bits[11].First<char>().ToString(), 16));
+            translatedData.Add("FEStateBitField ", Convert.ToInt32(bits[11].Last<char>().ToString(), 16));
+
+            return translatedData;
+        }
+
+        private static Dictionary<string, int> TranslateDataPage25(string[] bits)
+        {
+            Dictionary<string, int> translatedData = new Dictionary<string, int>();
+
+            translatedData.Add("PageID", 25);
+            translatedData.Add("Eventcounter" , Convert.ToInt32(bits[5], 16));
+            translatedData.Add("InstantaneousCadence " , Convert.ToInt32(bits[6], 16));
+            translatedData.Add("AccumulatedPower " , Convert.ToInt32(bits[7] + bits[8], 16));
+            translatedData.Add("InstantaneousPower " , Convert.ToInt32(bits[9] + bits[10].First<char>(), 16));
+            translatedData.Add("TrainerStatusbitField " , Convert.ToInt32(bits[10].Last<char>().ToString(), 16));
+            translatedData.Add("flagsBitField " , Convert.ToInt32(bits[11].First<char>().ToString(), 16));
+            translatedData.Add("FEStateBitField " , Convert.ToInt32(bits[11].Last<char>().ToString(), 16));
+
+            return translatedData;
+        }
+    }
+}
